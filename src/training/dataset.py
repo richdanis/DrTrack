5eb57a31f19ad49a3a4f5226e397f3da5e48ed64
Richard Danis <rdanis@ethz.ch>
2023-11-07 15:04:37 +0100

    First successful euler run.

---
+++ src/training/dataset.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+
+from torch.utils.data import Dataset
+
+class CellDataset(Dataset):
+
+    # TODO: currently my patches are of size 40x40.
+    # Should be turned into parameter.
+
+    def __init__(self, dataset_path, both_channels=False):
+
+        self.dataset = np.load(dataset_path)
+        # format: (N, 2, C, H, W)
+        # N: number of patches
+        # Second dimension: input and label (in this order)
+        # C: number of channels
+        # H: height
+        # W: width
+
+        # turn into torch tensor
+        self.dataset = torch.from_numpy(self.dataset)
+
+        # turn into float
+        self.dataset = self.dataset.float()
+        
+        # remove one channel if necessary
+        if not both_channels:
+            self.dataset = self.dataset[:,:,:1,:,:]
+
+    def __len__(self):
+
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+
+        # TODO: Augmentation?
+
+        return self.dataset[idx][0], self.dataset[idx][1]
---
+++ src/training/train.py
@@ -0,0 +1,118 @@
+import torch
+import argparse
+import torchvision
+import logging
+from info_nce import InfoNCE
+from tqdm import tqdm
+from dataset import CellDataset
+from models.efficientnet import EfficientNet
+
+
+def main():
+
+    # TODO: wandb integration
+
+    parser = argparse.ArgumentParser()
+    args = get_args(parser)
+
+    logging.basicConfig(filename='logs/training.log', encoding='utf-8', level=logging.DEBUG)
+
+    # load dataset
+    train_dataset = CellDataset(args.data_path + 'train.npy')
+    logging.info(f"Dataset loaded.")
+
+    # data loader
+    train_loader = torch.utils.data.DataLoader(train_dataset, \
+                                              batch_size=args.batch_size, \
+                                              shuffle=True)
+    
+    # load model
+    model = load_model()
+    model = model.to(args.device)
+    logging.info(f"Model loaded.")
+
+    # log number of parameters
+    logging.info(f"Number of parameters: {sum(p.numel() for p in model.parameters())}")
+
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    # loss criterion
+    # https://github.com/RElbers/info-nce-pytorch
+    # this is just a first try, can maybe use different contrastive loss
+    criterion = InfoNCE()
+
+    for epoch in range(args.epochs):
+
+        train_one_epoch(model, train_loader, optimizer, criterion, epoch, args)
+
+        break
+
+def train_one_epoch(
+        model: torch.nn.Module,
+        train_dl: torch.utils.data.DataLoader,
+        optimizer: torch.optim.Optimizer,
+        criterion: torch.nn.Module,
+        epoch: int,
+        args: argparse.Namespace
+    ):
+        # TODO: Logging, keeping track of loss, etc.
+        # TODO: Progressbar
+
+        # log epoch
+        logging.info(f"Epoch {epoch}")
+
+        model.train()
+
+        for x,y in train_dl:
+
+            x, y = x.to(args.device), y.to(args.device)
+
+            embeddings_x = model(x)
+            embeddings_y = model(y)
+
+            output = criterion(embeddings_x, embeddings_y)
+
+            optimizer.zero_grad()
+            output.backward()
+            optimizer.step()
+
+            print("got here :)")
+
+            break
+
+def evaluate(
+        model: torch.nn.Module,
+        val_dl: torch.utils.data.DataLoader,
+        criterion: torch.nn.Module,
+        epoch: int,
+        args: argparse.Namespace
+    ):
+     
+     # TODO: how to evaluate? Maybe just use accuracy.
+     
+     pass
+
+def get_args(parser):
+
+    # TODO: add more arguments
+
+    parser.add_argument('--data_path', type=str, required=True, help='Path to data.')
+    parser.add_argument('--model', default=None, help='Which model to use. Default is None.')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size. Default is 32.')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs. Default is 100.')
+    parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate. Default is 0.001.')
+    parser.add_argument('--device', type=str, default='cuda', help='Device to use. Default is cuda.')
+
+    return parser.parse_args()
+
+def load_model():
+
+    # TODO: needs to be changed if we want to be able to load different models.
+    # Currently using EfficientNet, version b1, lightweight: 6.5 million parameters.
+    # Output embedding has 1280 dimensions.
+    return EfficientNet()
+    
+
+if __name__ == "__main__":
+    main()
