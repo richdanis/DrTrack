c9ea2d8262f7b0740e2b626bbf8a4b9f1022c6c7
michavol <michavol@ethz.ch>
2023-11-28 14:45:57 +0100

    restructure the source code and integrate image splitting

--- src/preprocess/for_detection.py
+++ src/preprocess/for_detection.py
@@ -8,9 +8,12 @@ from skimage.morphology import disk
 from skimage.util import img_as_ubyte
 
 from .raw_image_reader import get_image_as_ndarray
+from .raw_image_reader import get_image_cut_as_ndarray
 
 
-def preprocess_for_detection(cfg, image_path: str, pixel: int = -1) -> np.ndarray:
+def preprocess_for_detection(cfg, 
+                            image_path: str, 
+                            pixel: int = -1) -> np.ndarray:
     """
     Preprocess the BF channel of a image
     ----------
@@ -53,11 +56,87 @@ def preprocess_for_detection(cfg, image_path: str, pixel: int = -1) -> np.ndarra
 
     return image
 
+def preprocess_cut_for_detection(cfg, 
+                            image_path: str, 
+                            upper_left_corner: tuple,
+                            pixel_dimensions: tuple,
+                            pixel: int = -1) -> np.ndarray:
+    """
+    Preprocess the BF channel of a cut of an nd2 image
+    ----------
+    Parameters:
+    image_path: str
+        relative path to the image (image as .nd2 file)
+    pixels: int
+        num of pixels to cut out of the full image, if -1 the full image is taken
+    ----------
+    Returns:
+    ndarray: 4d numpy array (uint16 so be careful)
+        with the following axes: Frames, Channels ['BF', 'DAPI'], Y (rows) and X (cols),
+        where the BF channel has been processed with quantile and locally histogram equalized
+    """
+    image = get_image_cut_as_ndarray(cfg,
+                                     ['BF', 'DAPI'], 
+                                     image_path, 
+                                     upper_left_corner,
+                                     pixel_dimensions,
+                                     all_frames=True, 
+                                     all_channels=False, 
+                                     frames = None,
+                                     pixel=pixel)
 
-def raw_to_preprocessed_for_detection(cfg, raw_image_path: Path, image_name: str, PREPROCESSED_PATH: Path,
+    # For each frame, preprocess channels inplace
+    image[:, 0, :, :] = np.uint16(2 ** 16 - (np.int32(image[:, 0, :, :]) + 1))
+
+    # Progress bar only if verbose is set to True
+    disable = True
+    if cfg.verbose:
+        disable = False
+
+    for frame in tqdm(image, desc='BF Preprocessing for Detection', disable=disable):
+        # Brightfield preprocessing
+        bf_chan = np.float64(frame[0, :, :])
+        bf_chan_low = np.quantile(bf_chan, 0.1)
+        bf_chan_high = np.quantile(bf_chan, 0.995)
+        bf_chan = np.clip((bf_chan - bf_chan_low) / (bf_chan_high - bf_chan_low), 0.0, 1.0)
+
+        pullback_min = bf_chan.min()
+        pullback_max = bf_chan.max()
+        bf_pullback = (bf_chan - pullback_min) / (pullback_max - pullback_min)
+
+        bf_pullback = np.clip((bf_pullback - np.quantile(bf_pullback, 0.5)) / (1.0 - np.quantile(bf_pullback, 0.5)),
+                              0.0, 1.0)
+
+        equalized = rank.equalize(img_as_ubyte(bf_pullback), footprint=disk(10)) / 255.0
+
+        bf_pullback = bf_pullback * equalized
+
+        smoothed = cv.GaussianBlur(bf_pullback, (3, 3), 0)
+        frame[0, :, :] = np.uint16(smoothed * (2 ** 16 - 1))
+
+    return image
+
+
+def raw_to_preprocessed_for_detection(cfg, 
+                                      raw_image_path: Path, 
+                                      image_name: str, 
+                                      PREPROCESSED_PATH: Path,
                                       pixel: int = -1) -> np.ndarray:
     """Preprocesses the raw .nd2 image for detection and saves it as .npy file."""
     preprocessed_image = preprocess_for_detection(cfg, raw_image_path, pixel=pixel)
     path = Path(PREPROCESSED_PATH / f"preprocessed_drpdtc_{image_name}.npy")
+    np.save(path, preprocessed_image)
+    return preprocessed_image
+
+def raw_cut_to_preprocessed_for_detection(cfg, 
+                                           raw_image_path: Path, 
+                                           upper_left_corner: tuple,
+                                           pixel_dimensions: tuple,
+                                           image_name: str, 
+                                           preprocessed_path: Path,
+                                           pixel: int = -1) -> np.ndarray:
+    """Preprocesses a cut of the raw .nd2 image for detection and saves it as .npy file."""
+    preprocessed_image = preprocess_cut_for_detection(cfg, raw_image_path, upper_left_corner, pixel_dimensions, pixel=pixel)
+    path = Path(preprocessed_path / f"preprocessed_drpdtc_{image_name}.npy")
     np.save(path, preprocessed_image)
     return preprocessed_image
--- src/track_droplets.py
+++ src/track_droplets.py
@@ -2,37 +2,36 @@ import os
 import time
 import argparse
 from pathlib import Path
+import nd2
 
 from preprocess.for_detection import raw_to_preprocessed_for_detection
 from preprocess.for_embeddings import raw_to_preprocessed_for_embeddings
+from preprocess.for_detection import raw_cut_to_preprocessed_for_detection
+from preprocess.for_embeddings import raw_cut_to_preprocessed_for_embeddings
+from preprocess.for_all import preprocess_all_cuts_and_store
+
+
 from utils.globals import *
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-# Run: python track_droplets.py "Small mvt 1.nd2" --pixel 100 -e
-
-def preprocess(cfg, raw_image_path: Path, image_name: str, pixel=-1):
-    """Preprocess the .nd2 images and save as .npy arrays."""
-    raw_to_preprocessed_for_detection(cfg, raw_image_path, image_name, PREPROCESSED_PATH, pixel=pixel)
-    raw_to_preprocessed_for_embeddings(cfg, raw_image_path, image_name, PREPROCESSED_PATH, pixel=pixel)
-
 @hydra.main(config_path="conf", config_name="config", version_base=None)
 def main(cfg: DictConfig):
+     # Start timer
      start_time = time.time()
 
-     raw_image_path = Path(RAW_PATH / cfg.preprocess.raw_image)
+     # Preprocess the image cuts if required
+     # Check conf/preprocess.yaml for settings
      image_name = cfg.preprocess.raw_image[:-4].lower().replace(' ', '_')
+     preprocessed_path_img = Path(PREPROCESSED_PATH / image_name)
 
-     if cfg.preprocess.pixel > 0:
-          image_name = image_name + f'_{cfg.preprocess.pixel}'
+     # Create paths if they do not exist
+     if not os.path.exists(preprocessed_path_img): 
+          os.makedirs(preprocessed_path_img) 
 
      if not cfg.preprocess.skip:
-          print("----Preprocess the Dataset for Detection and Embedding Creation----")
-          preprocess(cfg, raw_image_path, image_name, pixel=cfg.preprocess.pixel)
-     else:
-          print("----Skipping Preprocessing the Dataset----")
-
-
+         cut_names = preprocess_all_cuts_and_store(cfg, RAW_PATH, preprocessed_path_img, image_name)
+   
 if __name__ == '__main__':
     main()
