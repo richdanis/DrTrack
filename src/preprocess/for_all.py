e30187637cae0df79bdf4e3fa25d07f2d5a01fbc
michavol <michavol@ethz.ch>
2023-11-28 16:59:17 +0100

    make changes to config files

--- src/preprocess/for_all.py
+++ src/preprocess/for_all.py
@@ -13,7 +13,7 @@ def preprocess_cut_and_store(cfg, raw_image_path: Path, upper_left_corner: tuple
     raw_cut_to_preprocessed_for_detection(cfg, raw_image_path, upper_left_corner, pixel_dimensions, image_name, PREPROCESSED_PATH, pixel=pixel)
     raw_cut_to_preprocessed_for_embeddings(cfg, raw_image_path, upper_left_corner, pixel_dimensions, image_name, PREPROCESSED_PATH, pixel=pixel)
 
-def preprocess_all_cuts_and_store(cfg, RAW_PATH:str, PREPROCESSED_PATH: str, image_name: str):
+def preprocess_all_cuts_and_store(cfg, RAW_PATH:str, preprocessed_path: str, image_name: str):
     """
     Preprocess the .nd2 images seperated into cuts and save as .npy arrays.
     """
@@ -21,42 +21,41 @@ def preprocess_all_cuts_and_store(cfg, RAW_PATH:str, PREPROCESSED_PATH: str, ima
     raw_image_path = Path(RAW_PATH / cfg.preprocess.raw_image)
 
     # Preprocess the image cuts if required
-    if not cfg.preprocess.skip:
-        # Open nd2 file and check sizes
-        f = nd2.ND2File(raw_image_path)
-        size_y = f.sizes['Y']
-        size_x = f.sizes['X']
+    # Open nd2 file and check sizes
+    f = nd2.ND2File(raw_image_path)
+    size_y = f.sizes['Y']
+    size_x = f.sizes['X']
 
-        # Close nd2 file
-        f.close()
+    # Close nd2 file
+    f.close()
 
-        # Get desired numbers of cuts in each dimension
-        cuts_y = cfg.preprocess.cuts_y
-        cuts_x = cfg.preprocess.cuts_x
+    # Get desired numbers of cuts in each dimension
+    cuts_y = cfg.preprocess.cuts_y
+    cuts_x = cfg.preprocess.cuts_x
 
-        # Cut image into smaller images
-        cut_size_y = size_y // cuts_y
-        cut_size_x = size_x // cuts_x
-        cut_pixel_dimensions = (cut_size_y, cut_size_x)
+    # Cut image into smaller images
+    cut_size_y = size_y // cuts_y
+    cut_size_x = size_x // cuts_x
+    cut_pixel_dimensions = (cut_size_y, cut_size_x)
 
-        if cfg.verbose:
-            print("===================================================================")
-            print("Preprocess the Dataset for Detection and Embedding Creation\n")
-            print(f'Nr frames: {f.sizes["T"]}')
-            print(f'Nr channels: {f.sizes["C"]}')
-            print(f'Image dimensions: {cuts_y}x{cuts_x}')
-            print(f'Number of cuts: {cuts_y * cuts_x}')
-            print(f'Cut size: {cut_size_y}x{cut_size_x}\n')
-        
-        # Process and store each cut
-        cut_image_names = []
-        for i in range(cuts_y):
-            for j in range(cuts_x):
-                image_name_curr = f'{image_name}_y{i*cut_size_y}_x{j*cut_size_x}' # naming s.t. image name, (y, x, stride)
-                upper_left_corner = (i * cut_size_y, j * cut_size_x)
-                preprocess_cut_and_store(cfg, raw_image_path, upper_left_corner, cut_pixel_dimensions, image_name_curr, PREPROCESSED_PATH, pixel=cfg.preprocess.pixel)
+    if cfg.verbose:
+        print("===================================================================")
+        print("Preprocess the Dataset for Detection and Embedding Creation\n")
+        print(f'Nr frames: {f.sizes["T"]}')
+        print(f'Nr channels: {f.sizes["C"]}')
+        print(f'Image dimensions: {cuts_y}x{cuts_x}')
+        print(f'Number of cuts: {cuts_y * cuts_x}')
+        print(f'Cut size: {cut_size_y}x{cut_size_x}\n')
+    
+    # Process and store each cut
+    cut_image_names = []
+    for i in range(cuts_y):
+        for j in range(cuts_x):
+            image_name_curr = f'{image_name}_y{i*cut_size_y}_x{j*cut_size_x}' # naming s.t. image name, (y, x, stride)
+            upper_left_corner = (i * cut_size_y, j * cut_size_x)
+            preprocess_cut_and_store(cfg, raw_image_path, upper_left_corner, cut_pixel_dimensions, image_name_curr, preprocessed_path, pixel=cfg.preprocess.pixel)
 
-                # Save image names in list
-                cut_image_names.append(image_name_curr)
+            # Save image names in list
+            cut_image_names.append(image_name_curr)
 
-        return cut_image_names
+    return cut_image_names
--- src/track_droplets.py
+++ src/track_droplets.py
@@ -16,22 +16,60 @@ from utils.globals import *
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
+def create_dir(path):
+    if not os.path.exists(path): 
+        os.makedirs(path)
+
 @hydra.main(config_path="conf", config_name="config", version_base=None)
 def main(cfg: DictConfig):
      # Start timer
      start_time = time.time()
 
-     # Preprocess the image cuts if required
-     # Check conf/preprocess.yaml for settings
+     # Get name of image to be processed
      image_name = cfg.preprocess.raw_image[:-4].lower().replace(' ', '_')
-     preprocessed_path_img = Path(PREPROCESSED_PATH / image_name)
 
-     if not cfg.preprocess.skip:
-         # Create paths if they do not exist
-          if not os.path.exists(preprocessed_path_img): 
-               os.makedirs(preprocessed_path_img) 
+     ### PREPROCESSING ###
+     # Check conf/preprocess.yaml for settings
+     image_preprocessed_path = Path(PREPROCESSED_PATH / image_name)
 
-          cut_names = preprocess_all_cuts_and_store(cfg, RAW_PATH, preprocessed_path_img, image_name)
-   
+     if not cfg.skip_preprocessing:
+          # Create paths if they do not exist
+          create_dir(image_preprocessed_path)
+          cut_names = preprocess_all_cuts_and_store(cfg, RAW_PATH, image_preprocessed_path, image_name)
+
+     ### DROPLET EXTRACTION ###
+     # Check conf/extract_droplets.yaml for settings
+     image_droplets_path = Path(DROPLET_PATH / image_name)
+
+     if not cfg.skip_droplet_extraction:
+          # Create paths if they do not exist
+          create_dir(image_droplets_path) 
+
+          for filename in os.listdir(image_droplets_path):
+               f = os.path.join(image_droplets_path, filename)
+               # checking if it is a file
+               if os.path.isfile(f):
+                    print(f)
+
+
+     ### FEATURE EXTRACTION ###
+     # Check conf/extract_features.yaml for settings
+     image_embeddings_path = Path(FEATURE_PATH / image_name)
+
+     if not cfg.skip_feature_extraction:
+          # Create paths if they do not exist
+          create_dir(image_embeddings_path)
+
+
+
+     ### TRACKING ###
+     image_results_path = Path(RESULT_PATH / image_name)
+
+     if not cfg.skip_tracking:
+          # Create paths if they do not exist
+          create_dir(image_results_path)
+     
+
+
 if __name__ == '__main__':
     main()
