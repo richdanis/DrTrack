5947fb055dec18f12d2fac3fdac6913eb656124f
werkaaa <weronika.ormaniec@gmail.com>
2023-12-17 10:32:51 +0100

    Fix patches extraction.

--- src/evaluate/generate_paired_patches.py
+++ src/evaluate/generate_paired_patches.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
 
+
 # Used with paired_path = "c334_unstimulated_crop4x4.npy"
 def generate_paired_patches(paired_path, data_path):
-    
     patches = np.load("c334_unstimulated_crop4x4.npy")
 
     num_droplets = patches.shape[0]
     num_frames = patches.shape[1]
-    channel_to_extract = 0
 
     droplet_patches = {'droplet_id': [], 'frame': [], 'patch': []}
 
@@ -16,8 +15,11 @@ def generate_paired_patches(paired_path, data_path):
         for j in range(num_droplets):
             droplet_patches["droplet_id"].append(j)
             droplet_patches["frame"].append(i)
-            droplet_patches["patch"].append(patches[j, i, channel_to_extract, :, :])
-        
-    # And then save with np.save("paired_patches.npy", droplet_patches, allow_pickle=True)
+            droplet_patches["patch"].append(patches[j, i, :, :, :])
+
+    np.save("paired_patches.npy", droplet_patches, allow_pickle=True)
 
     return droplet_patches
+
+
+generate_paired_patches('', '')
