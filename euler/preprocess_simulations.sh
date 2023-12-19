2d26580a7695ad4b60ed1f14e78c33243ef664db
Richard Danis <rdanis@ethz.ch>
2023-12-19 10:30:57 +0100

    Adapt for 20k simulations :crossed_swords:

--- euler/preprocess_simulations.sh
+++ euler/preprocess_simulations.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #SBATCH --time=01:00:00
-#SBATCH --mem-per-cpu=10G
+#SBATCH --gpus=rtx_4090:1
+#SBATCH --mem-per-cpu=20G
 #SBATCH --job-name=simulations_preprocess
 #SBATCH --output=/cluster/home/%u/DrTrack/logs/%x.out                                                                         
 #SBATCH --error=/cluster/home/%u/DrTrack/logs/%x.err
 
 module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
 
-export JAX_PLATFORMS=cpu
+export JAX_PLATFORMS=cuda
 
 $HOME/$1/bin/python3.11 $HOME/DrTrack/src/evaluate_tracking.py \
     data_path=/cluster/scratch/$USER/data/evaluation \
@@ -17,9 +18,9 @@ $HOME/$1/bin/python3.11 $HOME/DrTrack/src/evaluate_tracking.py \
     skip_tracking=true \
     skip_results_generation=true \
     extract_visual_embeddings=droplets_all \
-    simulated_image=small_mvt_6000_droplets.csv \
+    simulated_image=small_mvt_20000_droplets.csv \
     evaluate=unbalanced_v1 \
-    device=cpu
+    device=cuda
 
 $HOME/$1/bin/python3.11 $HOME/DrTrack/src/evaluate_tracking.py \
     data_path=/cluster/scratch/$USER/data/evaluation \
@@ -28,9 +29,9 @@ $HOME/$1/bin/python3.11 $HOME/DrTrack/src/evaluate_tracking.py \
     skip_tracking=true \
     skip_results_generation=true \
     extract_visual_embeddings=droplets_all \
-    simulated_image=medium_mvt_6000_droplets.csv \
+    simulated_image=medium_mvt_20000_droplets.csv \
     evaluate=unbalanced_v1 \
-    device=cpu
+    device=cuda
 
 $HOME/$1/bin/python3.11 $HOME/DrTrack/src/evaluate_tracking.py \
     data_path=/cluster/scratch/$USER/data/evaluation \
@@ -39,6 +40,6 @@ $HOME/$1/bin/python3.11 $HOME/DrTrack/src/evaluate_tracking.py \
     skip_tracking=true \
     skip_results_generation=true \
     extract_visual_embeddings=droplets_all \
-    simulated_image=large_mvt_6000_droplets.csv \
+    simulated_image=large_mvt_20000_droplets.csv \
     evaluate=unbalanced_v1 \
-    device=cpu
+    device=cuda
